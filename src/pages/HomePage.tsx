0c27c041a09ee2a570d6623c03cdcc1ab6340f23
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2024-09-15 18:25:29 +0200

    Revamp app bar and home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,18 @@ export function Home() {
   return (
     <>
       <div className={styles['background']} />
-      <div className={styles['page']} />
+      <div className={styles['page']}>
+        <div className={styles['container']}>
+          <img className={styles['ffviiec-logo']} src={new URL('../assets/ffviiec-logo.png', import.meta.url).href} />
+          <div className={styles['description']}>
+            This site is an information hub about FINAL FANTASY VII EVER CRISIS, a video game by SQUARE ENIX.
+            <br />
+            All rights to the images and content used belong to their respective owners.
+            <br />
+            © SQUARE ENIX Powered by Applibot, Inc.
+          </div>
+        </div>
+      </div>
     </>
   );
 }
