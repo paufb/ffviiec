8310aa5d43ec87e7a09632611edf8f3f25045ece
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2024-09-18 21:09:58 +0200

    Restructure weapon modal's overflow handling

--- src/components/WeaponModal.tsx
+++ src/components/WeaponModal.tsx
@@ -10,7 +10,7 @@ import { ATBBarCost } from './ATBBarCost.tsx';
 import { ElementIcon } from './ElementIcon.tsx';
 import { OverboostStars } from './OverboostStars.tsx';
 import { SigilIcon } from './SigilIcon.tsx';
-import styles from '../pages/WeaponsPage.module.css';
+import styles from './WeaponModal.module.css';
 
 interface WeaponModalProps {
   weaponName: keyof Weapons | null;
@@ -63,120 +63,120 @@ export function WeaponModal({ weaponName, weapon, character, cAbility, selectedO
   }
 
   return (
-    <dialog id="weapon-modal" className={styles['weapon-modal']}>
-      <header className={styles['weapon-modal-header']}>
+    <dialog id="weapon-modal" className={styles['modal']}>
+      <header className={styles['modal-header']}>
         Weapon Details
-        <svg className={styles['weapon-modal-header-close-button']} onClick={closeModal} height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+        <svg className={styles['close-button']} onClick={closeModal} height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
       </header>
-      <div className={styles['weapon-modal-body']}>
-        <div className={styles['weapon-modal-body-column-info']}>
-          <div className={styles['weapon-modal-body-column-info-header']}>
+      <div className={styles['modal-body']}>
+        <div className={styles['column-info']}>
+          <div className={styles['column-info-header']}>
             <img src={character.icon} alt="" />
-            <div className={styles['weapon-modal-body-column-info-header-title']}>
-              <div className={styles['weapon-modal-body-column-info-header-title-name']}>
+            <div className={styles['column-info-header-title']}>
+              <div className={styles['column-info-header-title-name']}>
                 {weaponName}
               </div>
-              <div className={styles['weapon-modal-body-column-info-header-title-overboost']}>
+              <div className={styles['column-info-header-title-overboost']}>
                 <OverboostStars level={displayedOverboostLevel} size="1.5rem" />
               </div>
-              <div className={styles['weapon-modal-body-column-info-header-title-level']}>
+              <div className={styles['column-info-header-title-level']}>
                 Lv. {selectedWeaponLevel}
               </div>
             </div>
           </div>
-          <div className={styles['weapon-modal-body-column-info-stats']}>
+          <div className={styles['column-info-stats']}>
             Stats
             <hr />
-            <div className={styles['weapon-modal-body-column-info-stats-row']}>
-              <div className={styles['weapon-modal-body-column-info-stats-row-title']}>
+            <div className={styles['column-info-stats-row']}>
+              <div className={styles['column-info-stats-row-title']}>
                 <img src={HPIcon} alt="" />
                 HP
               </div>
-              <div className={styles['weapon-modal-body-column-info-stats-row-value']}>
+              <div className={styles['column-info-stats-row-value']}>
                 --
               </div>
             </div>
-            <div className={styles['weapon-modal-body-column-info-stats-row']}>
-              <div className={styles['weapon-modal-body-column-info-stats-row-title']}>
+            <div className={styles['column-info-stats-row']}>
+              <div className={styles['column-info-stats-row-title']}>
                 <img src={PATKIcon} alt="" />
                 PATK
               </div>
-              <div className={styles['weapon-modal-body-column-info-stats-row-value']}>
+              <div className={styles['column-info-stats-row-value']}>
                 {getWeaponPAtk(weapon, displayedOverboostLevel)}
               </div>
             </div>
-            <div className={styles['weapon-modal-body-column-info-stats-row']}>
-              <div className={styles['weapon-modal-body-column-info-stats-row-title']}>
+            <div className={styles['column-info-stats-row']}>
+              <div className={styles['column-info-stats-row-title']}>
                 <img src={MATKIcon} alt="" />
                 MATK
               </div>
-              <div className={styles['weapon-modal-body-column-info-stats-row-value']}>
+              <div className={styles['column-info-stats-row-value']}>
                 {getWeaponMAtk(weapon, displayedOverboostLevel)}
               </div>
             </div>
-            <div className={styles['weapon-modal-body-column-info-stats-row']}>
-              <div className={styles['weapon-modal-body-column-info-stats-row-title']}>
+            <div className={styles['column-info-stats-row']}>
+              <div className={styles['column-info-stats-row-title']}>
                 <img src={PDEFIcon} alt="" />
                 PDEF
               </div>
-              <div className={styles['weapon-modal-body-column-info-stats-row-value']}>
+              <div className={styles['column-info-stats-row-value']}>
                 --
               </div>
             </div>
-            <div className={styles['weapon-modal-body-column-info-stats-row']}>
-              <div className={styles['weapon-modal-body-column-info-stats-row-title']}>
+            <div className={styles['column-info-stats-row']}>
+              <div className={styles['column-info-stats-row-title']}>
                 <img src={MDEFIcon} alt="" />
                 MDEF
               </div>
-              <div className={styles['weapon-modal-body-column-info-stats-row-value']}>
+              <div className={styles['column-info-stats-row-value']}>
                 --
               </div>
             </div>
-            <div className={styles['weapon-modal-body-column-info-stats-row']}>
-              <div className={styles['weapon-modal-body-column-info-stats-row-title']}>
+            <div className={styles['column-info-stats-row']}>
+              <div className={styles['column-info-stats-row-title']}>
                 <img src={HEALIcon} alt="" />
                 HEAL
               </div>
-              <div className={styles['weapon-modal-body-column-info-stats-row-value']}>
+              <div className={styles['column-info-stats-row-value']}>
                 {getWeaponHeal(weapon, displayedOverboostLevel)}
               </div>
             </div>
           </div>
         </div>
-        <div className={styles['weapon-modal-body-column-middle']}>
-          <div className={styles['weapon-modal-body-column-middle-image']}>
+        <div className={styles['column-middle']}>
+          <div className={styles['column-middle-image']}>
 
           </div>
-          <div className={styles['weapon-modal-body-column-middle-controls']}>
-            <div className={styles['weapon-modal-body-column-middle-controls-previous']} onClick={decreaseTier}>
+          <div className={styles['column-middle-controls']}>
+            <div className={styles['column-middle-controls-previous']} onClick={decreaseTier}>
               <svg style={{ transform: 'scaleX(-1)' }} width="24" height="24" viewBox="0 0 24 24"><path d="M10.477 0h-8.977l12.024 12-12.024 12h8.977l12.023-12z"/></svg>
             </div>
-            <div className={styles['weapon-modal-body-column-middle-controls-tier']}>
+            <div className={styles['column-middle-controls-tier']}>
               {renderCurrentTier()}
             </div>
-            <div className={styles['weapon-modal-body-column-middle-controls-next']} onClick={increaseTier}>
+            <div className={styles['column-middle-controls-next']} onClick={increaseTier}>
               <svg width="24" height="24" viewBox="0 0 24 24"><path d="M10.477 0h-8.977l12.024 12-12.024 12h8.977l12.023-12z"/></svg>
             </div>
           </div>
         </div>
-        <div className={styles['weapon-modal-body-column-abilities']}>
-          <div className={styles['weapon-modal-body-column-abilities-cability']}>
+        <div className={styles['column-abilities']}>
+          <div className={styles['column-abilities-cability']}>
             C. Ability
             <hr />
-            <div className={styles['weapon-modal-body-column-abilities-cability-container']}>
-              <div className={styles['weapon-modal-body-column-abilities-cability-container-header']}>
-                <div className={styles['weapon-modal-body-column-abilities-cability-container-header-icons']}>
-                  <img src="" alt="" className={styles['weapon-modal-body-column-abilities-cability-container-header-icons-cability']} />
-                  <ElementIcon element={weapon.element} className={styles['weapon-modal-body-column-abilities-cability-container-header-icons-element']} />
-                  <SigilIcon sigil={cAbility.sigil} className={styles['weapon-modal-body-column-abilities-cability-container-header-icons-sigil']} />
+            <div className={styles['column-abilities-cability-container']}>
+              <div className={styles['column-abilities-cability-container-header']}>
+                <div className={styles['column-abilities-cability-container-header-icons']}>
+                  <img src="" alt="" className={styles['column-abilities-cability-container-header-icons-cability']} />
+                  <ElementIcon element={weapon.element} className={styles['column-abilities-cability-container-header-icons-element']} />
+                  <SigilIcon sigil={cAbility.sigil} className={styles['column-abilities-cability-container-header-icons-sigil']} />
                 </div>
                 {weapon.cAbility}
-                <div className={styles['weapon-modal-body-column-abilities-cability-container-header-atb']}>
+                <div className={styles['column-abilities-cability-container-header-atb']}>
                   <ATBBarCost cost={cAbility.atbCost} />
                 </div>
               </div>
               <hr />
-              <div className={styles['weapon-modal-body-column-abilities-cability-container-description']}>
+              <div className={styles['column-abilities-cability-container-description']}>
                 {getWeaponCAbility(weapon, displayedOverboostLevel)}
               </div>
             </div>
