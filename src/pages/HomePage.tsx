bb087faaf45246956ae3cf8d8eadd87970bf389c
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2024-10-30 22:01:00 +0100

    Add navigation anchor buttons in home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
+import WeaponsIcon from '../assets/ui/enhance.png';
 import pageAnimations from './page-animations.module.css';
 import styles from './HomePage.module.css';
+import { NavLink } from 'react-router-dom';
 
 export function Home() {
   return (
@@ -14,6 +16,16 @@ export function Home() {
             All rights to the assets and content used belong to their respective owners.
             <br />
             © SQUARE ENIX Powered by Applibot, Inc.
+          </div>
+        </div>
+        <div className={`${styles['anchors-container']} ${pageAnimations['fade-in-from-bottom']}`}>
+          <div className={styles['anchor-container']}>
+            <div className={styles['outer-shadow']} />
+            <div className={styles['inner-shadow']} />
+            <div className={styles['border']} />
+            <NavLink to={'/weapons'} className={styles['anchor']}>
+              <img src={WeaponsIcon} />
+            </NavLink>
           </div>
         </div>
       </div>
